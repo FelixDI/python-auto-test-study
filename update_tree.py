ee9caed01f35178248316ad80fdedc43c0a9054d
FelixDI <cuihedi@hotmail.com>
2026-05-25 12:18:29 +0800

    feat: 排除 mysql_data 和 jenkins_home

--- update_tree.py
+++ update_tree.py
@@ -38,7 +38,7 @@ def generate_tree() -> str:
         result = subprocess.run(
             [
                 "tree",
-                "-I", "__pycache__|*.pyc|.git|.idea|.DS_Store|venv|env|.venv|mysql_data|jenkins_home",
+                "-I", "__pycache__|*.pyc|.git|.idea|.DS_Store|venv|env|.venv|mysql_data|jenkins_home",   # 排除
                 "--dirsfirst",
                 "-F"
             ],
