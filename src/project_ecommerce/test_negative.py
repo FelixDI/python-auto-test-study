466fbc8f3672b7995a3dd5b3bf803bfad0b0e514
FelixDI <cuihedi@hotmail.com>
2026-05-27 00:40:25 +0800

    添加最终Allure测试报告

---
+++ src/project_ecommerce/test_negative.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Time        : 2026-05-26 19:50
+# @Author      : Felix Cui
+# @Email       : cuihediyzu@gmail.com
+# @File        : test_negative.py
+# @PythonEnv   : Python 3.12 (pythonautotest-py312)
+# @IDE         : PyCharm
+# @Description : 
---
+++ src/project_ecommerce/test_orders.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Time        : 2026-05-26 17:44
+# @Author      : Felix Cui
+# @Email       : cuihediyzu@gmail.com
+# @File        : test_orders.py
+# @PythonEnv   : Python 3.12 (pythonautotest-py312)
+# @IDE         : PyCharm
+# @Description : 
