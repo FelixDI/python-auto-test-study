01870552e4e9e0e771f612c10610fa52efa017c7
FelixDI <cuihedi@hotmail.com>
2026-06-08 12:06:04 +0800

    fxi: 添加功能、菜单组件、避免循环导入问题

--- src/saucedemo_ui_test/pages/cart_page.py
+++ src/saucedemo_ui_test/pages/cart_page.py
@@ -13,6 +13,7 @@
 
 from src.saucedemo_ui_test.common.base_page import BasePage
 from src.saucedemo_ui_test.pages.checkout_page import CheckoutPage
+from src.saucedemo_ui_test.components.menu_component import MenuComponent
 
 
 class CartPage(BasePage):
@@ -23,9 +24,9 @@ class CartPage(BasePage):
     CHECKOUT_BUTTON = "#checkout"
     CONTINUE_SHOPPING_BUTTON = "#continue-shopping"
 
-    # def __init__(self, page):
-    #     super().__init__(page)
-    #     self.menu = MenuComponent(page)
+    def __init__(self, page):
+        super().__init__(page)
+        self.menu = MenuComponent(page)
 
     # 高内聚低耦合：每个页面对象只负责自己的操作，不依赖其他页面  进入cart界面 实际上是products页面的按钮功能
     # cart_page这里只需要定义一个 验证方法assert_page_loaded 被实例调用
@@ -33,14 +34,24 @@ class CartPage(BasePage):
         self.assert_text_contains(self.PAGE_TITLE, "Your Cart")
         self.assert_url_contains("/cart.html")
 
+    def get_cart_item_names(self) -> str:
+        items = self.page.locator(self.CART_ITEM_NAME)
+        return [item.text_content() for item in items]
+
+    def remove_item_by_index(self, index: int):
+        self.page.locator(self.REMOVE_BUTTON).click()
+
     def get_cart_items_count(self) -> int:
         return self.page.locator(self.CART_ITEM).count()
 
-    def go_to_checkout(self):
+    def go_to_checkout(self) -> CheckoutPage:
         self.click(self.CHECKOUT_BUTTON)
+        return CheckoutPage(self.page)
 
     def click_continue_shopping(self):
         self.click(self.CONTINUE_SHOPPING_BUTTON)
+        from src.saucedemo_ui_test.pages.products_page import ProductsPage  # 避免循环导入  采用延迟导入
+        return ProductsPage(self.page)
 
 
 # # pages/cart_page.py
