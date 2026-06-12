c9a59d03cdc05ebed5d0a33f1108829206bc3c0e
FelixDI <cuihedi@hotmail.com>
2026-06-12 16:58:48 +0800

    fix: TABLE order_items

--- src/ecommerce_api_test/conftest.py
+++ src/ecommerce_api_test/conftest.py
@@ -65,7 +65,8 @@ def init_db(db_util):
 def clean_database_before_test(db_util):
     # 用order_items存储每个订单的商品明细(商品 ID、购买数量、下单单价) 这个table能执行一个订单购买数个商品
     # table orders 在fastapi后端玩具demo代码实现，目前只是简单的一个订单购买一个商品
-    tables = {"order_items", "orders", "products", "users"}
+    # tables = {"order_items", "orders", "products", "users"}
+    tables = {"orders", "products", "users"}
 
     db_util.execute("SET FOREIGN_KEY_CHECKS = 0")  # 临时关闭外键约束 允许删除/清空有外键关联的表
     for table in tables:
